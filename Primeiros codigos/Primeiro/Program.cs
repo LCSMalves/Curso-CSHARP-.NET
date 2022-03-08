8156ac525fbd11efc3d3991648764c36b2347add
Malves <lmalves10@gmail.com>
2022-03-08 11:10:11 -0300

    criação do repositório
    criação do repositório

---
+++ Primeiros codigos/Primeiro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace PrimeiroProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 1;
+
+            var b = "Hello";
+
+            Console.WriteLine(b);
+
+            object c = "teste";
+
+            Console.WriteLine(c);
+
+            c = 2;
+
+            Console.WriteLine(c);
+
+            if (a <= 1)
+            {
+                Console.WriteLine("Menor que 1");
+            }
+            else
+            {
+                Console.WriteLine("maior que 1");
+            }
+        }
+    }
+}
